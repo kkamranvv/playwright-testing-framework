1d1f8263ac0cebf04ae5ca47d1f0c4c4d749169e
Kamran <musadirlikamran@gmail.com>
2026-01-05 20:32:43 +0100

    comment out OrangeHRM tests

--- tests/ui/orangehrm/Sidebar/activeMenu.spec.ts
+++ tests/ui/orangehrm/Sidebar/activeMenu.spec.ts
@@ -1,14 +1,14 @@
-import { test, expect } from "../../../../fixtures/orangehrm/fixtures.js";
-import { Sidebar } from "../../../../pages/orangehrm/Sidebar/Sidebar.js";
+// import { test, expect } from "../../../../fixtures/orangehrm/fixtures.js";
+// import { Sidebar } from "../../../../pages/orangehrm/Sidebar/Sidebar.js";
 
-test("Active sidebar item is highlighted", async ({ login }) => {
-  const sidebar = new Sidebar(login);
+// test("Active sidebar item is highlighted", async ({ login }) => {
+//   const sidebar = new Sidebar(login);
 
-  await login.getByRole("link", { name: "PIM" }).click();
+//   await login.getByRole("link", { name: "PIM" }).click();
 
-  await expect(login).toHaveURL(/.*pim/);
+//   await expect(login).toHaveURL(/.*pim/);
 
-  await expect(sidebar.getMenuItem("PIM")).toHaveClass(/active/);
+//   await expect(sidebar.getMenuItem("PIM")).toHaveClass(/active/);
 
-  expect(await sidebar.isActive("PIM")).toBeTruthy();
-});
+//   expect(await sidebar.isActive("PIM")).toBeTruthy();
+// });
--- tests/ui/orangehrm/Sidebar/sidebarCollapse.spec.ts
+++ tests/ui/orangehrm/Sidebar/sidebarCollapse.spec.ts
@@ -1,18 +1,18 @@
-import { test, expect } from "../../../../fixtures/orangehrm/fixtures.js";
-import { Sidebar } from "../../../../pages/orangehrm/Sidebar/Sidebar.js";
+// import { test, expect } from "../../../../fixtures/orangehrm/fixtures.js";
+// import { Sidebar } from "../../../../pages/orangehrm/Sidebar/Sidebar.js";
 
-test("Sidebar collapses and expands", async ({ login }) => {
-  const sidebar = new Sidebar(login);
+// test("Sidebar collapses and expands", async ({ login }) => {
+//   const sidebar = new Sidebar(login);
 
-  expect(await sidebar.isCollapsed()).toBeFalsy();
+//   expect(await sidebar.isCollapsed()).toBeFalsy();
 
-  await sidebar.collapse();
-  await expect(async () => {
-    expect(await sidebar.isCollapsed()).toBeTruthy();
-  }).toPass();
+//   await sidebar.collapse();
+//   await expect(async () => {
+//     expect(await sidebar.isCollapsed()).toBeTruthy();
+//   }).toPass();
 
-  await sidebar.collapse();
-  await expect(async () => {
-    expect(await sidebar.isCollapsed()).toBeFalsy();
-  }).toPass();
-});
+//   await sidebar.collapse();
+//   await expect(async () => {
+//     expect(await sidebar.isCollapsed()).toBeFalsy();
+//   }).toPass();
+// });
